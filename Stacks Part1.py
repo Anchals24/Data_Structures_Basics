75fbace4eca75a510b5753734803cb9cf0d3203a
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-06-05 11:26:07 +0530

    Update Stacks Part1.py

--- Stacks Part1.py
+++ Stacks Part1.py
@@ -1,31 +1,39 @@
-
 #Implementation of a Stack using Lists 
+
+#In this , I have simply implemented stacks using lists...
+#Step1 : Let's define a class stack....
 
 class Stack:
     def __init__(self):
+        #Let's initialize an empty list....... We will use list to store the data here.
         self.elements = []
 
     def leng(self):
+        #Similar to lists : "len()" has been used to determine the length of the list.
         return len(self.elements)
     
+    #Function to check whether the stack is empty or not.
     def is_empty(self):
         return len(self.elements) == 0
     
+    #Function to insert the data into the stack...
     def push(self,data):
         self.elements.append(data)
         print(self.elements)
     
+    #Function to delete data from the stack....
     def pop(self):
         if self.is_empty():
             print("Stack is empty")
             return
         return self.elements.pop()
-
+    
+    #Function to find out the top element of the stack.......
     def top(self):
         if self.is_empty():
             print("Stack is empty")
             return
-        return self.elements[-1]
+        return self.elements[-1] #similar to find the last element of the list using negative indexing.
     
     
 stack = Stack()
