4f7f6633ee086bd07344841c46d4e2e983109c7c
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-06-09 15:48:31 +0530

    Implementing queue using List

--- Queue Part1.py
+++ Queue Part1.py
@@ -1,23 +1,33 @@
 #Implementing Queue using Lists:
 
+#Create a class Queue.....
 class Queue:
+    #Initialize and create an empty list at the very beginning which will be used to store the queue data.
     def __init__(self):
         self.elements = []
     
+    #Check the length of the queue.
     def leng(self):
         return len(self.elements)
-
+    
+    #Check whether the queue is empty or not...
     def is_empty(self):
         return len(self.elements) == 0
+    
+    #Inserting an element to the queue.....
     
     def enqueue(self , data):
         self.elements.append(data)
         return self.elements
     
+    #Deleting an element from the queue....
+    
     def dequeue(self):
         if len(self.elements) == 0:
             return -1
         return self.elements.pop(0)
+    
+    #Return the first element of the queue
 
     def first(self):
         if len(self.elements) == 0:
