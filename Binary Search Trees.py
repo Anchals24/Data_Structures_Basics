057b783bd70ddb6673d7a1ee97f13f2b150c6096
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-07-12 17:28:59 +0530

    Binary Search Trees.

--- Binary Search Trees.py
+++ Binary Search Trees.py
@@ -28,6 +28,57 @@ class Binarysearchtree:
                 temp.right = new
         else:
             self.root = new
+            
+   def _search_(self , key):
+        troot = self.root
+        if key == troot.data:
+            return True
+        elif key < troot.data:
+            troot = troot.left
+        elif key > troot.data:
+            troot = troot.right
+        return False
+
+    def __recursivesearch__(self , troot, key):
+        if troot:
+            if key == troot.data:
+                return True
+            elif key < troot.data:
+                return self.__recursivesearch__(troot.left , key)
+            elif key > troot.data:
+                return self.__recursivesearch__(troot.right , key)
+        else:
+            return False
+            
+    def __deletion__(self , key):
+        p = self.root
+        pp = None
+        while p and p.data != key:
+            pp = p
+            if key < p.data:
+                p = p.left
+            else:
+                p = p.right
+        if not p:
+            return False
+        if p.left and p.right:
+            maxi = p.left
+            while maxi.right:
+                maxi = maxi.right
+            p.data = maxi.data
+        c = None
+        if p.left:
+            c = p.left
+        else:
+            c = p.right
+        if p == self.root:
+            self.root = None
+        else:
+            if p == pp.left:
+                pp.left = c
+            else:
+                pp.right = c
+
 
     
 
