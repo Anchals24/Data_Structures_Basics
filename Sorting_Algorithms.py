a136ebe527c271e688a0472e1403ae7527a4cec3
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-09-06 11:54:58 +0530

    Sorting Algorithms : Well Explained.

--- Sorting_Algorithms.py
+++ Sorting_Algorithms.py
@@ -1,6 +1,8 @@
 #Count_Sort
 
+
 A = [3 , 5 , 8 , 9, 6 , 2 , 3 , 5 , 5]
+print("Unsorted Array is >> " , A)
 leng = len(A)
 maxi = max(A)
 count = []
@@ -11,10 +13,12 @@ for i in A:
     count[i] += 1
 lc = len(count)
 c = 0
+j = 0
 while c < lc:
     if count[c] == 0:
         c += 1
     else:
-        sortedd.append(c)
+        A[j] = c
+        j += 1
         count[c] -= 1
-print(sortedd)
+print("Sorted Array is >>" , A)
